2e271897b61764bcfb6f2d7f6519cc24b1acff62
ChaitaliTarate <104971680+ChaitaliTarate@users.noreply.github.com>
2022-05-27 16:23:15 +0530

    Classwork for joins ,having clause
    SQL Build in functons

--- SQLQuery Person.sql
+++ SQLQuery Person.sql
@@ -186,3 +186,183 @@ select *  from Person1 where personId<=3
 	select country,count (personId) as PersonCount from Person1
 	group by country
 
+
+	select * from Person1
+	select * from Orders
+
+	alter table orders add productname varchar(20)
+	alter table orders add price int
+
+	insert into Orders values(1,101,1,'Keyborad',999)
+	insert into Orders values(2,102,1,'mouse',799)
+	insert into Orders values(3,103,5,'laptop',23999)
+	insert into Orders values(4,104,5,'mobile',26000)
+	insert into Orders values(5,105,5,'led screen',11999)
+	insert into Orders values(6,106,9,'t shirt',999)
+	insert into Orders values(7,107,9,'mouse',999)
+
+
+--1.inner join
+	select p.name,p.country,o.orderNo,o.productname,o.price
+	from Person1 p
+	inner join Orders o on o.personId=p.personId;
+
+--2Left Join
+	select p.name,p.country,o.orderNo,o.productname,o.price,o.orderId
+	from Person1 p
+	left join Orders o on o.personId=p.personId;
+
+--3.Right Join
+	select p.name,p.country,o.orderNo,o.productname,o.price,o.orderId
+	from Person1 p
+	right join Orders o on o.personId=p.personId;
+
+--4.Full join
+	select p.name,p.country,o.orderNo,o.productname,o.price,o.orderId
+	from Person1 p
+	full join Orders o on o.personId=p.personId;
+
+	
+--5.self join
+	
+	create table Manager(
+	id int primary key,
+	name varchar(20)
+	)
+
+	select * from tblEmployee
+
+	alter table tblEmployee add managerid int
+	Update tblEmployee set managerid=1 where id in(2,3,4,5)
+	Update tblEmployee set managerid=6 where id in(7,8,9,10)
+	Update tblEmployee set managerid=11 where id in(12)
+
+	select e1.name as empName, e2.name as managerName
+	from tblEmployee e1,tblEmployee e2
+	where e1.managerid=e2.id
+
+	create table tblStudy(
+	studid varchar(5),
+	courseId varchar(5), 
+	year int
+	)
+	
+	
+	insert into tblStudy values('s1','c1',2016)
+	insert into tblStudy values('s2','c2',2017)
+	insert into tblStudy values('s1','c2',2017)
+
+
+
+	select s1.studid,s1.courseId,s1.year
+	from tblStudy s1,tblStudy s2
+	where s1.studid=s2.studid and s1.courseId<>s2.courseId and s1.year<>s2.year
+
+--Cross Join
+ alter table tblEmployee drop column deptName
+
+ create table dept(
+ did int primary key,
+ dname varchar(10)
+ )
+
+
+ alter table tblEmployee add did int
+
+
+ alter table tblEmployee add constraint fk_dept_tbleEmployee foreign key(did) references dept(did)
+
+	Update tblEmployee set did=1 where id in(2,3,4,5)
+	Update tblEmployee set did=4 where id in(7,8,9,10)
+	Update tblEmployee set did=7 where id in(12)
+
+	insert into dept values(1,'HR')
+	insert into dept values(2,'Developer')
+	insert into dept values(3,'testing')
+	insert into dept values(4,'account')
+	insert into dept values(5,'HR')
+
+
+	select e.name,e.salary,d.did,d.dname
+	from tblEmployee e
+	inner join dept d on d.did=e.did;
+
+	select e.name,e.salary,d.did,d.dname
+	from tblEmployee e
+	inner join dept d on d.did=e.did
+	where d.dname='HR'
+
+	
+	select e.name,e.salary,d.did,d.dname
+	from tblEmployee e
+	inner join dept d on d.did=e.did
+	where d.dname='HR' order by salary
+
+	select e.name,e.salary,d.did,d.dname
+	from tblEmployee e
+	inner join dept d on d.did=e.did
+	where d.dname='HR' order by salary desc
+
+	select e.name,e.salary,d.did,d.dname
+	from tblEmployee e
+	inner join dept d on d.did=e.did
+	where d.dname='account' order by name 
+
+
+	select e.name,e.salary,d.did,d.dname
+	from tblEmployee e
+	inner join dept d on d.did=e.did
+	where d.dname='HR' order by name,salary
+
+ select * from tblEmployee
+
+
+ select * from dept
+
+
+ --Union and Union All
+	select city from Customer where city='pune'
+	union all
+	select city from supllier where city='pune'
+
+--having clause
+	select country,count(personid)  as perosncount from Person1
+	group by country having count(personId)>5
+
+	select country ,count(personid)as personcount from Person1
+	group by country
+	having country in('india','USA')
+	order by count(personid)
+
+	select dname,count(did) as empcount from dept
+	group by dname
+	having dname in('HR','account')
+	order by count(did)
+
+--build in functions
+--substring,concat,len,upper,lower,trim ,ltrim,rtim,round ,replace,reverse
+
+	update tblEmployee set salary=34998.99 where id=1
+
+	select SUBSTRING(name,0,3) as tempname from tblEmployee where id=7
+	
+	select concat(name,'',city) as info from tblEmployee
+	
+	select name,LEN(name) as length from tblEmployee
+
+	select UPPER(name )as name from tblEmployee
+
+	select LOWER(name )as name from tblEmployee	
+
+	select ROUND(salary,2)as salary from tblEmployee
+
+	select ROUND(345.889,1)as roundvalue
+
+	select CAST(salary as varchar(20))from tblEmployee
+
+	select REVERSE(name) as reversename from tblEmployee
+
+	select salary,RANK() over (order by salary) as salaryrank from tblEmployee
+
+
+	select replace(name,'aarti','kirti') as info from tblEmployee where id=7
